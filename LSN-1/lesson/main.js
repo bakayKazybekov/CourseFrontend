9c66b8061410a908fb19d8baa6f8b04079f2eaf5
Bakay <bakaykazybekovich@gmail.com>
2023-08-07 20:51:15 +0600

    JS LSN-1 lesson

---
+++ LSN-1/lesson/main.js
@@ -0,0 +1,40 @@
+// console.log("Hello world");
+// alert("Massage in window");
+
+// confirm("Accept usloviya?")
+// prompt('Write your name');
+
+// var massage = 'Accept usloviya?';
+
+// confirm(massage);
+
+// let firstName = 'Bakay';
+// const age = 16;
+
+// console.log(firstName, age);
+
+// firstName = 'Baxa';
+// console.log(firstName);
+
+// age = 17;
+// console.log(age);
+
+// let name = prompt('Name?');
+// console.log(name);
+
+// let isActivate = confirm('Sign in?');
+// console.log(isActivate);
+
+let srting = 'Строка в ковычках';
+let number = 12345; 
+let boolean1 = true;
+let boolean2 = false;
+
+let und = undefined;
+
+let n = null;
+
+let obj = {
+  name: 'bakay',
+  age: 16
+};
