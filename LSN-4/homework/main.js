bc2ba013759981f0e811d65748ea0376eb13ded2
Bakay <bakaykazybekovich@gmail.com>
2023-08-17 20:03:56 +0600

    homework4

---
+++ LSN-4/homework/main.js
@@ -0,0 +1,124 @@
+// function someFn(arg) {
+//   return arg ** 3
+// }
+// console.log(someFn(4));
+
+
+// function func(num) {
+//   return num ** 2;
+// }
+// console.log(func(7));
+
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(2, 6));
+
+
+// function operation(a, b, c) {
+//   return (a - b) / c;
+// }
+// console.log(operation(40, 2, 2));
+
+
+// function week(a, b, c, d, e, f, g) {
+//   if (a === 1) {
+//     console.log('Понидельник');
+//   }
+//   if (b === 2) {
+//     console.log('Вторник');
+//   }
+//   if (c === 3) {
+//     console.log('Среда');
+//   }
+//   if (d === 4) {
+//     console.log('Четверг');
+//   }
+//   if (e === 5) {
+//     console.log('Пятница');
+//   }
+//   if (f === 6) {
+//     console.log('Суббота');
+//   }
+//   if (g === 7) {
+//     console.log('Воскресенье');
+//   }
+// }
+// week(1, 2, 3, 4, 5, 6, 7);
+
+
+// let array = [2, 7, 3, 8, 5, 10];
+// let arrayContain = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] === 5 ) {
+//     arrayContain = 1;
+//   }
+// }
+// if (arrayContain === 1) {
+//   console.log('да');
+// } else {
+//   console.log('нет');
+// }
+
+
+// let num = 2;
+// let check = false;
+// if (num <= 2) {
+//   check = false;
+// } else {
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       check = true;
+//       break;
+//     }
+//   }
+// }
+// console.log(check);
+
+
+// let array2 = [2, 7, 3, 8, 5, 10];
+// let arrayCheck = 0;
+// for (let i = 0; i < array2.length; i++) {
+//   if (array2[i] === array2[i - 1]) {
+//     arrayCheck = 1;
+//   }
+// }
+// if (arrayCheck === 1) {
+//   console.log('да');
+// } else {
+//   console.log('нет')
+// }
+
+
+// function func2(num1, num2) {
+//   if (num1 === num2) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(func2(2, 2));
+
+
+// function func3(num1, num2) {
+//   if (num1 + num2 > 10) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(func3(3, 8));
+
+
+// function func4(num) {
+//   if (num < 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(func4(2));
+
+
